2b263d04d1666ce84c309edd54614e9fd57917d5
Christoph Steiner <gawen@t-online.de>
2020-05-11 11:09:27 +0200

    api call

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
 const [data, setData] = useState()
 
 
-// API
+
+
+
+useEffect( () => {
+  // API CALL IMGFLIP
+      var myHeaders = new Headers();
+      myHeaders.append("Cookie", "__cfduid=d670a2bc303530def6e9a2be073c49ff11589186257; iflipsess=lp0ntvd14i9d83dboaklhlqa4i");
+
+      var requestOptions = {
+        method: 'GET',
+        headers: myHeaders,
+        redirect: 'follow'
+      };
+      const apiCall = async () => {
+      let call = await fetch("https://api.imgflip.com/get_memes", requestOptions)
+        .then(response => response.json())
+        .then(result => setData(result.data.memes))
+        .catch(error => console.log('error', error.message));
+      }
+      apiCall()
+},[]);
 
 
 
